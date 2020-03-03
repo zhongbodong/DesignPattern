d35f095d15f4465a1ce81f92ef408f351b0a5dc0
zhongbodong1994 <zhongbodong1994@foxmail.com>
2020-03-03 15:30:46 +0800

    设计模式学习

---
+++ DesignPatterns/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task.Run(()=>{
+                 for (int i = 0; i < 5; i++)
+                 {
+                     SingetonPattern.CreateSingeton();
+                 }
+
+            });
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
